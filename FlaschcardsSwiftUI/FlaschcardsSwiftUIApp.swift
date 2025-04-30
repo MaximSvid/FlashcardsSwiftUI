0560742a0d520446cbb0db52abe7f252efb47560
Maxim Svidrak <151841989+MaximSvid@users.noreply.github.com>
2025-04-30 13:02:48 +0200

    Initial Commit

---
+++ FlaschcardsSwiftUI/FlaschcardsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaschcardsSwiftUIApp.swift
+//  FlaschcardsSwiftUI
+//
+//  Created by Maxim Svidrak on 30.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FlaschcardsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
